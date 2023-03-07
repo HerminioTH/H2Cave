b6226dbe8974fa61c34f9f76ba2e5b175ccf50ec
Herminio <H.TasinafoHonorio@tudelft.nl>
2023-03-07 23:46:45 +0100

    Added creep models (dislocation and pressure solution)

--- libs/Elements.py
+++ libs/Elements.py
@@ -209,3 +209,94 @@ class DashpotElement(BaseElement):
 		self.eps_ie_old = local_projection(zero_tensor, self.TS)
 		self.eps_ie_rate = local_projection(zero_tensor, self.TS)
 		self.eps_ie_rate_old = local_projection(zero_tensor, self.TS)
+
+
+
+class DislocationCreep(BaseElement):
+	def __init__(self, fem_handler, settings, element_name="DislocationCreep"):
+		super().__init__(fem_handler)
+		self.element_name = element_name
+		self.theta = settings["Time"]["theta"]
+		self.__load_props(settings)
+		self.__initialize_tensors()
+
+	def build_A(self):
+		pass
+
+	def build_b(self, C):
+		b_form = inner(sigma(C, self.eps_ie), epsilon(self.v))*self.dx
+		self.b = assemble(b_form)
+
+	def update(self):
+		self.eps_ie_old.assign(self.eps_ie)
+		self.eps_ie_rate_old.assign(self.eps_ie_rate)
+
+	def compute_viscous_strain(self, stress, dt):
+		self.__compute_viscous_strain_rate(stress)
+		self.eps_ie.assign(local_projection(self.eps_ie_old + dt*(self.theta*self.eps_ie_rate_old + (1 - self.theta)*self.eps_ie_rate), self.TS))
+
+	def __compute_viscous_strain_rate(self, stress):
+		s = stress - (1./3)*tr(stress)*Identity(3)
+		von_Mises = sqrt((3/2.)*inner(s, s))
+		self.eps_ie_rate.assign(local_projection(self.B*(von_Mises**(self.n-1))*s, self.TS))
+
+	def __load_props(self, settings):
+		self.A = Constant(settings[self.element_name]["A"])
+		self.n = Constant(settings[self.element_name]["n"])
+		self.T = Constant(settings[self.element_name]["T"])
+		self.R = 8.32		# Universal gas constant
+		self.Q = 51600  	# Creep activation energy, [J/mol]
+		self.B = float(self.A)*np.exp(-self.Q/(self.R*float(self.T)))
+
+	def __initialize_tensors(self,):
+		zero_tensor = Expression((("0.0","0.0","0.0"), ("0.0","0.0","0.0"), ("0.0","0.0","0.0")), degree=0)
+		self.eps_ie = local_projection(zero_tensor, self.TS)
+		self.eps_ie_old = local_projection(zero_tensor, self.TS)
+		self.eps_ie_rate = local_projection(zero_tensor, self.TS)
+		self.eps_ie_rate_old = local_projection(zero_tensor, self.TS)
+
+
+class PressureSolutionCreep(BaseElement):
+	def __init__(self, fem_handler, settings, element_name="PressureSolutionCreep"):
+		super().__init__(fem_handler)
+		self.element_name = element_name
+		self.theta = settings["Time"]["theta"]
+		self.__load_props(settings)
+		self.__initialize_tensors()
+
+	def build_A(self):
+		pass
+
+	def build_b(self, C):
+		b_form = inner(sigma(C, self.eps_ie), epsilon(self.v))*self.dx
+		self.b = assemble(b_form)
+
+	def update(self):
+		self.eps_ie_old.assign(self.eps_ie)
+		self.eps_ie_rate_old.assign(self.eps_ie_rate)
+
+	def compute_viscous_strain(self, stress, dt):
+		self.__compute_viscous_strain_rate(stress)
+		self.eps_ie.assign(local_projection(self.eps_ie_old + dt*(self.theta*self.eps_ie_rate_old + (1 - self.theta)*self.eps_ie_rate), self.TS))
+
+	def __compute_viscous_strain_rate(self, stress):
+		s = stress - (1./3)*tr(stress)*Identity(3)
+		self.eps_ie_rate.assign(local_projection(self.B*s, self.TS))
+
+	def __load_props(self, settings):
+		self.A = Constant(settings[self.element_name]["A"])
+		self.n = Constant(settings[self.element_name]["n"])
+		self.d = Constant(settings[self.element_name]["d"])
+		# self.T = Constant(settings[self.element_name]["T"])
+		# self.R = 8.32		# Universal gas constant
+		# self.Q = 51600  	# Creep activation energy, [J/mol]
+		# self.B = float(self.A)*np.exp(-self.Q/(self.R*float(self.T)))
+		self.B = float(self.A)/(self.d**self.n)
+
+	def __initialize_tensors(self,):
+		zero_tensor = Expression((("0.0","0.0","0.0"), ("0.0","0.0","0.0"), ("0.0","0.0","0.0")), degree=0)
+		self.eps_ie = local_projection(zero_tensor, self.TS)
+		self.eps_ie_old = local_projection(zero_tensor, self.TS)
+		self.eps_ie_rate = local_projection(zero_tensor, self.TS)
+		self.eps_ie_rate_old = local_projection(zero_tensor, self.TS)
+		
